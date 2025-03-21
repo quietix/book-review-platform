76b6d137fc7a2fed5c15d059defc9b9b674061d8
Ivan Zashchyk <124403197+quietix@users.noreply.github.com>
2025-03-21 20:20:20 +0200

    feat: Add ratings (#8)
    * feat: add ratings
    
    * refactor: cleanup rating_service.py

--- backend/api/v1/router.py
+++ backend/api/v1/router.py
@@ -5,6 +5,7 @@ from .auth_api import router as auth_router
 from .authors_api import router as authors_router
 from .genres_api import router as genres_router
 from .books_api import router as books_router
+from .ratings_api import router as ratings_router
 
 
 router = APIRouter(prefix="/v1")
@@ -14,3 +15,4 @@ router.include_router(profiles_router, tags=["Profiles"])
 router.include_router(authors_router, tags=["Authors"])
 router.include_router(genres_router, tags=["Genres"])
 router.include_router(books_router, tags=["Books"])
+router.include_router(ratings_router, tags=["Ratings"])
